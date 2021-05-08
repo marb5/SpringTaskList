362111a9c7e0f8766667d8c93c1807e1e9e0df6f
Marcin Bielen <mbielen1995@gmail.com>
2021-05-08 19:21:34 +0200

    About changing url path name, delete http method forbidden

--- src/main/java/com/example/SpringTaskList/model/TaskRepository.java
+++ src/main/java/com/example/SpringTaskList/model/TaskRepository.java
@@ -2,12 +2,25 @@ package com.example.SpringTaskList.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  *
  * @author marcin
  */
+//edycja sciezki adresu url, zmiana opisu / nazwy "obiektu" w przesylanych pakietach
+//@RepositoryRestResource(path = "mytasks", collectionResourceRel = "mytasks")
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    @Override
+    //wylaczenie metody delete http, nie mozna usuwac przy pomocy taska
+    @RestResource(exported = false)
+    public void delete(Task task);
+
+    @Override
+    @RestResource(exported = false)
+    //jw, nie mozna usuwac przy pomocy id
+    public void deleteById(Integer id);
     
 }
